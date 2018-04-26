5ae3afacf62ec0a5d805312ecbe28248bbddf276
mark meyers <kilfour@gmail.com>
2018-04-26 11:47:20 +0200

    All

---
+++ QuickXml.Tests/AllByName.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace QuickXml.Tests
+{
+    public class AllByName
+    {
+        [Fact]
+        public void Content()
+        {
+            const string input =
+@"<root>
+    <tag1><target>target1</target></tag1>
+    <tag2><target>target2</target></tag2>
+    <tag2>
+        <tag3><target>target3</target></tag3>
+    </tag2>
+</root>";
+            var xmlParser = XmlParse.All("target").Content().Many();
+            var result = xmlParser.Parse(input).ToArray();
+            Assert.Equal("target1", result[0]);
+            Assert.Equal("target2", result[1]);
+            Assert.Equal("target3", result[2]);
+        }
+    }
+}
---
+++ QuickXml/All.cs
@@ -0,0 +1,19 @@
+﻿using QuickXml.UnderTheHood;
+
+namespace QuickXml
+{
+	public static partial class XmlParse
+	{
+		public static XmlParser<XmlParserNode> All(string tagName)
+		{
+			return state => 
+                state.Current.All(tagName)(state);
+		}
+
+		public static XmlParser<XmlParserNode> All(this XmlParser<XmlParserNode> parser, string tagName)
+		{
+			return state => 
+                parser(state).IfSuccessfull(result => result.Value.All(tagName)(state));
+		}
+	}
+}
